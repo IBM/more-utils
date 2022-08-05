b977ee98d118992af9fe68284da3269c61a6919c
Dhaval Salwala <Dhaval.Vinodbhai.Salwala@ibm.com>
2022-08-05 16:28:01 +0100

    Bump pandas version to 1.4.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "pycloudmessenger @ http://github.com/IBM/pycloudmessenger/archive/v0.7.3.tar.gz",
         "pika==0.13.0",
-        "pandas==1.4.1",
+        "pandas==1.4.3",
         "pyspark==3.2.1",
         "logzero",
         "ipykernel",
